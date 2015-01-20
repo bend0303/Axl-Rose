76a3ddea06cd0ef02279dfdb878dd52524ffb3db
Ben Diamant <ben@analoc.com>
2015-01-20 11:47:47 +0200

    Angular ui addition

--- config/default.js
+++ config/default.js
@@ -2,7 +2,8 @@
 
 module.exports = {
     data: {
-        url: 'http://storage.googleapis.com/structure.rollout.io/development/547c40e01b78cfe60b2b5a62/547c45011b78cfe60b2b5a64/i386/fd31c5695f18fb49134da652cf47d19d'
+        url: 'http://storage.googleapis.com/structure.rollout.io/development/547c40e01b78cfe60b2b5a62/547c45011b78cfe60b2b5a64/i386/fd31c5695f18fb49134da652cf47d19d',
+        url1: 'http://storage.googleapis.com/structure.rollout.io/development/53ff41229a15ba0000000004/540d70434d9cbc6b82000f43/i386/412c2c19f8336ea14aae8ad1a32944ce'
     }
 
 }
--- server/services/fdata.service.js
+++ server/services/fdata.service.js
@@ -5,13 +5,13 @@ var fdataHandler = require('../handlers/fdata.handler'),
 
 module.exports.listFunctionsData = function (callback) {
     request({
-        url: config.data.url,
+        url: config.data.url1,
         json: true
     }, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            if (typeof body == Array && body.length > 0) {
+            if (body.length && body.length > 0) {
                 fdataHandler.buildDataObject(body, callback);
             } else return callback("Data source is wrong or empty");
         } else callback(error)
     })
-}
+};
